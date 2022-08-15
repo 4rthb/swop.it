588740872b6c880915aee30f699645dd6103dad0
Tiago Schmidt <schmidttiago7@gmail.com>
2022-08-15 14:23:19 -0300

    Adding initial scheme for login.

--- client/src/App0.js
+++ client/src/App0.js
@@ -1,30 +1,38 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
-	async function loginUser(event) {
+	async function registerUser(event) {
 		event.preventDefault()
 
-		const response = await fetch('http://localhost:5000/api/loginUser', {
+		const response = await fetch('http://localhost:5000/api/registerUser', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
+				name,
 				email,
 				password,
 			}),
 		})
-
-		const data = await response.json()
 	}
 
 	return (
 		<div>
-			<h1>Login</h1>
-			<form onSubmit={loginUser}>
+			<h1>Register</h1>
+			<form onSubmit={registerUser}>
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					placeholder="Name"
+				/>
+				<br />
 				<input
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
@@ -39,7 +47,7 @@ function App() {
 					placeholder="Password"
 				/>
 				<br />
-				<input type="submit" value="Login" />
+				<input type="submit" value="Register" />
 			</form>
 		</div>
 	)
--- server/server.js
+++ server/server.js
@@ -1,9 +1,68 @@
 const express = require('express');
+var fs = require('fs');
+var users_data = fs.readFileSync('./models/users.json');
+var users = JSON.parse(users_data);
+const cors = require('cors');
+
+console.log(users);
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 app.get('/api', (req, res) => {
-  res.json({"users": ["one", "two", "three", "five"]});
+  res.json({
+    "users": ["one", "two", "three", "five"]
+  });
 });
+
+app.post('/api/registerUser', async (req, res) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  users[email] = {name,password}
+  var data = JSON.stringify(users, null, 2);
+  fs.writeFile('./models/users.json', data, finished);
+
+  function finished(err) {
+    reply = {
+      name: name,
+      email: email,
+      status: 'success'
+    };
+    res.send(reply);
+  }
+})
+
+app.post('/api/loginUser', async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var validated = false;
+
+  Object.entries(users).forEach((entry) => {
+    const [key, value] = entry;
+    if(email === key){
+      if(value['password'] === password){
+        validated = true;
+      }else{
+        validated = false;
+      }
+    }else{
+      validated = false;
+    }
+
+    if(validated){
+      reply = {
+        status: 'success'
+      };
+      res.send(reply);
+    }else{
+      reply = {
+        status: 'fail'
+      };
+      res.send(reply);
+    }
+  });
+})
 
 app.listen(5000, () => console.log(`Listening on port ${5000}`));
