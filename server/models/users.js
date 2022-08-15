359c96f4f941d31c8d28b7e0be233f7b9f30e4a4
Arthur P Baumgardt <b_arthp@outlook.com>
2022-08-15 14:16:12 -0300

    basic server config

---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+export const users = [
+    {
+        id: "UserID",
+        name: "UserName",
+        email: "UserEmail",
+        password: "UserPassword",
+        date: "RegisterDate",
+        products: ["ProductID1", "ProductID2"]
+    },
+    {
+        id: "",
+        name: "",
+        password: "",
+        date: "",
+        products: [""]
+    }
+]
--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
+import { products } from './controllers/productController';
+import { validateUser } from './controllers/userController';
+
 const express = require('express');
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.json({"users": ["one", "two", "three", "five"]});
+app.post('/', (req, res) => {
+  // validating user
+  const { username, password } = req.body;
+  if(validateUser(username, password)) res.redirect('/marketplace');
+  else res.send('Invalid user info!');
+});
+
+app.post('marketplace/new', (req, res) => {
+  const { username, productname } = req.body
+  // ...
+});
+
+app.get('/marketplace', (req, res) => {
+  // get data from products.json and send it to marketplace
+  res.json(products);
 });
 
 app.listen(5000, () => console.log(`Listening on port ${5000}`));
