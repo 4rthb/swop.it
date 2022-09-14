2e0c9cb5eb5f062a738571112a85ce3ca910e369
Tiago Schmidt <schmidttiago7@gmail.com>
2022-09-14 10:11:21 -0300

    Adding answer trade API.

--- server/routers/offerRouter.js
+++ server/routers/offerRouter.js
@@ -15,25 +15,29 @@ offerRouter.post(
     const itemId = req.params.itemId;
     const item = await Item.findById(itemId);
 
-    if (item) {
-      const offer = new Offer({
-        itemDesired: item.id,
-        itemsOffered: req.body.itemsOffered,
-        offerState: "PENDING",
-        desiredOwner: item.owner,
-        offeredOwner: req.body.user_id,
-      });
 
+    let offeredItems = [];
+    if (item) {
       for (var itemIndex in req.body.itemsOffered) {
         const newItem = await Item.findById(req.body.itemsOffered[itemIndex]);
 
         if (newItem) {
-          newItem.currentState = "BLOCKED";
-          newItem.image = "url.boa.com";
-          const savedItem = await newItem.save();
+          if (newItem.currentState !== "BLOCKED") {
+            newItem.currentState = "BLOCKED";
+            newItem.image = "url.boa.com";
+            const savedItem = await newItem.save();
+            offeredItems.push(savedItem.id);
+          }
         }
       }
 
+      const offer = new Offer({
+        itemDesired: item.id,
+        itemsOffered: offeredItems,
+        offerState: "PENDING",
+        desiredOwner: item.owner,
+        offeredOwner: req.body.user_id,
+      });
 
       const createdOffer = await offer.save();
       res.send({
@@ -43,6 +47,70 @@ offerRouter.post(
     } else {
       res.status(404).send({
         message: "Item inexistente."
+      });
+    }
+  })
+);
+
+offerRouter.post(
+  '/answer/:offerId/',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const offerId = req.params.offerId;
+    const offer = await Offer.findById(offerId);
+
+    if (offer.desiredOwner.toString() === req.user._id) {
+      const answer = req.body.answer;
+
+      let receivedItems = [];
+      if (answer === "ACCEPTED") {
+        if (offer.offerState === "PENDING") {
+          for (var itemIndex in offer.itemsOffered) {
+            const offeredItem = await Item.findById(offer.itemsOffered[itemIndex].toString());
+
+            if (offeredItem) {
+              offeredItem.currentState = "AVAILABLE";
+              offeredItem.owner = offer.desiredOwner.toString();
+              const received = await offeredItem.save();
+              receivedItems.push(received);
+            }
+          }
+
+          const desiredItem = await Item.findById(offer.itemDesired);
+          desiredItem.owner = offer.offeredOwner;
+          const given = await desiredItem.save();
+
+          offer.offerState = "FINISHED";
+          const finishedOffer = await offer.save();
+          res.send({
+            _id: offer._id,
+            receivedItems: receivedItems,
+          });
+        } else {
+          res.status(404).send({
+            message: "Proposta já rejeitada."
+          });
+        }
+      } else {
+        offer.offerState = "REJECTED";
+        const rejectedOffer = await offer.save();
+
+        for (var itemIndex in offer.itemsOffered) {
+          const offeredItem = await Item.findById(offer.itemsOffered[itemIndex].toString());
+
+          if (offeredItem) {
+            offeredItem.currentState = "AVAILABLE";
+            const received = await offeredItem.save();
+          }
+        }
+
+        res.send({
+          offer: rejectedOffer,
+        });
+      }
+    } else {
+      res.status(404).send({
+        message: "Proposta não encontrada."
       });
     }
   })
